11c3764f7508d05aeb1f01e7fefed95e141ab7ba
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-09-25 09:57:23 +0530

    To illustrate the concept of constructor overloading in java.

---
+++ ConstructorOverloading3.java
@@ -0,0 +1,18 @@
+//whenever we are writing any argument constructor in parent class
+//it is highly recommended to write no argument constructor also
+
+class Parent
+{
+  Parent(){}
+
+  Parent(int i){}
+}
+
+class ConstructorOverloading3 extends Parent
+{
+   public static void main(String[] args)
+   {
+
+   }
+ }
+
