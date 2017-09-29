1dd647dba2add78239e97f9944fde4bd697cce5c
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-09-29 10:02:51 +0530

    To illustrate the concept of overriding in java.

---
+++ Overriding2.java
@@ -0,0 +1,21 @@
+class Parent
+{ 
+  public Object mth()
+  {
+    return null;
+  }
+}
+
+class Overriding1 extends Parent
+{ 
+  public String mth()// child class method return type need not be same as parent method return type its child type also allowed
+  {
+    return null;
+  }
+  
+  public static void main(String []args)
+  {
+    Overriding1 o= new Overriding1();
+    o.mth();
+   }
+}
