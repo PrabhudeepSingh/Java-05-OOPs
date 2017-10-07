193ef2cd8391a816ed1df2f190939a34054ad996
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-10-07 11:22:46 +0530

    To illustrate the concept of static control flow in java.

---
+++ StaticControl10.java
@@ -0,0 +1,12 @@
+class StaticControl10
+{
+  static StaticControl10 s= new StaticControl10();
+  
+  StaticControl10()
+  {  
+    System.out.println("Constructor.");
+    System.exit(0);
+  }
+}
+
+//RE:Main method not found in class StaticControl10
