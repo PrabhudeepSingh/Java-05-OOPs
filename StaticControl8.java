63412c43a36d52a4ade9b75e294516c0e506e1b2
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-10-07 11:22:12 +0530

    To illustrate the concept of static control flow in java.

---
+++ StaticControl8.java
@@ -0,0 +1,14 @@
+class StaticControl8
+{  
+
+  static int x= mth();
+
+  public static int mth() 
+  {  
+   System.out.println("Static Method.");
+   System.exit(0);
+   return 10;
+  }
+}
+
+//RE: Main method not found in class StaticControl8.
